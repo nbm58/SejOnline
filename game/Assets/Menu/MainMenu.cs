28fd32958499b793b8073701a7dbeecda9a58f12
tam499 <tam499@nau.edu>
2023-02-19 15:24:47 -0700

    Local Game
    added local gameplay functionality based on Patrick Kelley's implementation

--- game/Assets/Menu/MainMenu.cs
+++ game/Assets/Menu/MainMenu.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button playButton;
+    public Button optionsButton;
+    public Button quitButton;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Local Play Scene Loaded");
     }
 
     public void QuitGame()
