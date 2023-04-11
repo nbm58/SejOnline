d2423ec56b855a72ebdd3a7daf28f6d5949c81fc
Johnathan Ray <jdr479@nau.edu>
2023-04-10 23:26:42 -0700

    Made changes to game UI, including a 'pop-up' menu for game instructions

--- SejOnline/Game/Assets/Game/GameUI.cs
+++ SejOnline/Game/Assets/Game/GameUI.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class GameUI : MonoBehaviour
 {
     public GameObject instructions;
+    public Transform coverPosition;
 
     public void toggleInstructions()
     {
         if (instructions.activeSelf)
         {
             instructions.SetActive(false);
+            coverPosition.position = new Vector3(6.0f, -750.0f, 0.0f);
+
             return;
         }
+        
         instructions.SetActive(true);
+        coverPosition.position = new Vector3(485.0f, 260.0f, 0.0f);
     }
 }
