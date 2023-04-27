dddd52e442b12b94260f11395625c8982c12d8b5
tam499 <tam499@nau.edu>
2023-04-27 15:42:53 -0700

    improve game ui
    added ui elements for:
    quit confirmation - game now prompts users to confirm after pressing the quit button
    how to play - improved instructions visual
    about - updated about message with improved visual
    
    all buttons now act as toggles for their respective ui elements
    toggling any of the three elements will also toggle the others to ensure no overlapping ui elements

--- SejOnline/Game_clone_0/Assets/Menu/ButtonController.cs
+++ SejOnline/Game_clone_0/Assets/Menu/ButtonController.cs
@@ -7,6 +7,10 @@ public class ButtonController : MonoBehaviour
     public AudioSource buttonClick;
     public AudioSource rollSound;
 
+    [SerializeField] private GameObject instructionsDisplay;
+    [SerializeField] private GameObject aboutDisplay;
+     [SerializeField] private GameObject quitConfirmationDisplay;
+
     public void PlayButton()
     {
         buttonClick.Play();
@@ -15,5 +19,47 @@ public class ButtonController : MonoBehaviour
     public void PlayRoll()
     {
         rollSound.Play();
+    }
+
+    public void toggleInstructions()
+    {
+        if (instructionsDisplay.activeSelf)
+        {
+            instructionsDisplay.SetActive(false);
+        }
+        else
+        {
+            instructionsDisplay.SetActive(true);
+            aboutDisplay.SetActive(false);
+            quitConfirmationDisplay.SetActive(false);
+        }
+    }
+
+    public void toggleAbout()
+    {
+        if (aboutDisplay.activeSelf)
+        {
+            aboutDisplay.SetActive(false);
+        }
+        else
+        {
+            aboutDisplay.SetActive(true);
+            instructionsDisplay.SetActive(false);
+            quitConfirmationDisplay.SetActive(false);
+        }
+    }
+
+    public void toggleQuitConfirmation()
+    {
+        if (quitConfirmationDisplay.activeSelf)
+        {
+            quitConfirmationDisplay.SetActive(false);
+        }
+        else
+        {
+            quitConfirmationDisplay.SetActive(true);
+            instructionsDisplay.SetActive(false);
+            aboutDisplay.SetActive(false);
+        }
     }
 }
